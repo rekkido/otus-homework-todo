9ae4a4d910834fb139074a64723777f8d41ac747
Dmitrii <dikkerd@ya.ru>
2023-11-28 18:14:13 +0300

    Hw02 todo (#1)
    * Add Tests for ToDo app
    
    * Add Allure Report
    
    * Add Allure Report
    
    * Review fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.20"
     application
+    id("io.qameta.allure") version "2.11.2"
 }
 
 group = "org.example"
@@ -15,10 +16,13 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+    implementation("io.github.serpro69:kotlin-faker:1.15.0")
+    implementation("io.qameta.allure:allure-junit-platform:2.24.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy("allureReport")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,65 @@
+import data.Priority
+import data.Task
+import data.TasksRepository
+import data.TasksRepositoryMemory
+import io.github.serpro69.kfaker.Faker
+import io.qameta.allure.Description
+import kotlin.test.BeforeTest
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class MainTest {
+    private lateinit var testRepository: TasksRepository
+    private val faker = Faker()
+
+    private fun createNewTask(repository: TasksRepository, faker: Faker): Int {
+        val name = faker.cowboyBebop.character()
+        val priority = Priority.values().random()
+        return repository.addTask(Task(name = name, priority = priority))
+    }
+
+
+    @BeforeTest
+    fun populateTasks() {
+        val tasksRepositoryMemory = TasksRepositoryMemory()
+        val iterations = faker.random.nextInt(2, 5)
+        for (i in 1..iterations) {
+            createNewTask(tasksRepositoryMemory,faker)
+        }
+        testRepository = tasksRepositoryMemory
+    }
+
+    @Test
+    @Description("Добавление задачи и появление ее в списке")
+    fun addTaskAndCheckAppearanceTest() {
+        val newTaskId = createNewTask(testRepository,faker)
+        val lastIdFromTaskList = testRepository.getTasks().last().id
+        assertEquals(newTaskId, lastIdFromTaskList)
+    }
+
+    @Test
+    @Description("Завершить задачу и проверить корректность работы фильтра по завершенным задачам")
+    fun checkFinishFilterAfterFinishTaskTest() {
+        val newTaskId = createNewTask(testRepository,faker)
+        testRepository.completeTask(newTaskId)
+        val listOfALLTasks = testRepository.getTasks(completed = true).size
+        val listOfActiveTasks = testRepository.getTasks(completed = false).size
+        assertEquals(1, (listOfALLTasks-listOfActiveTasks), "Expected 1 task in filter, but got ")
+    }
+
+    @Test
+    @Ignore
+    @Description("Проверить сортировку задач по названию")
+    fun checkSortingByNameTest() {
+        // Sorting is not implemented yet
+    }
+
+    @Test
+    @Ignore
+    @Description("Проверить сортировку задач по сроку исполнения")
+    fun checkSortingByDueTimeTest() {
+        // Sorting is not implemented yet
+    }
+}
